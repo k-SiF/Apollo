83e54872db407608acbffb43575d031c8bcf6c9a
Sif <s.akaaboune@student.maastrichtuniversity.nl>
2026-06-13 01:13:46 +0200

    Fix double copy bug

--- src/shader.h
+++ src/shader.h
@@ -4,8 +4,13 @@ class Shader {
     public:
         Shader(const char* filepath);
         ~Shader();
+        
+        Shader(const Shader&) = delete;
+        Shader& operator=(const Shader&) = delete;
+
         void use() const;
         unsigned int compile(unsigned int type, const std::string& source);
+
 
     private:
         unsigned int m_id = 0;
