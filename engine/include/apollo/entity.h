5e73610f41c42ade86f356735d32589ea9bded0c
k-SiF <sifeddine.akaaboune03@gmail.com>
2026-06-21 03:34:14 +0200

    Improved/Fixed collision check and added fixedUpdate.

--- engine/include/apollo/entity.h
+++ engine/include/apollo/entity.h
@@ -5,11 +5,24 @@
 #include <glm/gtc/matrix_transform.hpp> 
 
 namespace apollo {
+    class Scene;
+
+    struct Collider {
+        glm::vec2 min, max;
+        bool collision;
+        bool intersect(const Collider& other) const {
+            return min.x < other.max.x && max.x > other.min.x &&
+            min.y < other.max.y && max.y > other.min.y;
+        }
+    };
+
     class Entity {
         public:
             Entity(Mesh* mesh, glm::vec2 position = glm::vec2(0.0f));
             virtual ~Entity();
-            virtual void update();
+            virtual void update(float deltaTime);
+            virtual void fixedUpdate(float deltaTime);
+            virtual void onCollision(Entity& other) {}
 
             bool isAlive() const { return m_alive; }
             void destroy() { m_alive = false; }
@@ -18,24 +31,40 @@ namespace apollo {
             void setPosition(glm::vec2 position) { m_position = position; }
             glm::vec2 getScale() const { return m_scale; }
             void setScale(glm::vec2 scale) { m_scale = scale; }
+            glm::vec2 getColScale() const { return m_colScale; }
+            void setColScale(glm::vec2 colScale) { m_colScale = colScale; }
             float getRotation() const { return m_rotation; }
             void setRotation(float rotation) { 
                 if (rotation >= 360.0f || rotation <= -360.0f) rotation = 0.0f;
                 m_rotation = rotation;
             }
+            Collider getBounds() const;
+            Collider getBoundsAt(glm::vec2 position) const;
             Mesh* getMesh() const { return m_mesh; }
             void setMesh(Mesh* mesh) { m_mesh = mesh; }
             Texture* getTexture() const { return m_tex; }
             void setTexture(Texture* tex) { m_tex = tex; }
+            void setTextureSize(Texture* tex, float size);
+
+            Scene* getScene() const { return m_scene; }
             
-            glm::mat4 getModelMatrix() const;
-        
+            glm::vec2 getRenderPosition(float alpha) const {
+                return m_prevPosition * (1.0f - alpha) + m_position * alpha;   // lerp
+            }
+            glm::mat4 getModelMatrix(float alpha) const;
+            
         protected:
             glm::vec2 m_position;
+            glm::vec2 m_prevPosition;
             glm::vec2 m_scale = glm::vec2(1.0f);
+            glm::vec2 m_colScale = glm::vec2(1.0f);
+
             float m_rotation = 0.0f;
+            bool m_alive = true;
+
             Mesh* m_mesh = nullptr;
             Texture* m_tex = nullptr;
-            bool m_alive = true;
+            Scene* m_scene = nullptr;
+            friend class Scene;
     }; 
 } 
--- engine/src/entity.cpp
+++ engine/src/entity.cpp
@@ -1,21 +1,41 @@
 #include <apollo/entity.h>
+#include "entity.h"
 
 namespace apollo {
-    Entity::Entity(Mesh *mesh, glm::vec2 position) : m_position(position), m_mesh(mesh) {
+    Entity::Entity(Mesh *mesh, glm::vec2 position) : m_position(position), m_prevPosition(position) ,m_mesh(mesh) {
     }
 
     Entity::~Entity() {
     }
 
-    void Entity::update() {
+    void Entity::update(float deltaTime) { 
 
     }
 
-    glm::mat4 Entity::getModelMatrix() const {
-        glm::mat4 model = glm::mat4(1.0f);                                      // identity
-        model = glm::translate(model, glm::vec3(m_position, 0.0f));             // position
+    void Entity::fixedUpdate(float deltaTime) {
+
+    }
+
+    void Entity::setTextureSize(Texture* tex, float size) {
+        m_tex = tex;
+        float aspect = (float)tex->width() / tex->height();
+        m_scale = glm::vec2(aspect, 1.0f) * size;
+    }
+
+    glm::mat4 Entity::getModelMatrix(float alpha) const {
+        glm::vec2 pos = m_prevPosition * (1.0f - alpha) + m_position * alpha;
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(pos, 0.0f));// position
         model = glm::rotate(model, glm::radians(m_rotation), glm::vec3(0,0,1)); // rotation (around z, for 2D)
-        model = glm::scale(model, glm::vec3(m_scale, 1.0f));                    // scale
+        model = glm::scale(model, glm::vec3(m_scale, 1.0f));          // scale
         return model;
+    }
+
+    Collider Entity::getBounds() const {
+        return getBoundsAt(m_position);
+    }
+
+    Collider Entity::getBoundsAt(glm::vec2 position) const {
+        glm::vec2 offset = glm::abs(m_scale) * 0.5f * m_colScale;
+        return { position - offset, position + offset};
     }
 }
