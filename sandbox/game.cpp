ae4315acc2698b377d636d9dbfd1676a17d5ed47
k-SiF <sifeddine.akaaboune03@gmail.com>
2026-06-21 03:56:42 +0200

    Added interpolated camera follow, collision box debugdraw toggle, and fullscreen toggle.

--- sandbox/game.cpp
+++ sandbox/game.cpp
@@ -1,24 +1,32 @@
 #include "game.h"
 #include <iostream>
 
+
 Game::Game() {}
 
 void Game::start() {
     m_player = m_scene.addEntity<Player>(&m_quad);
-    m_player->setScale(glm::vec2(0.2f, 0.2f)); 
+    m_player->setPosition(glm::vec2(1.5f));
 
     setBackground(0.1f, 0.1f, 0.15f);
 
-    for (int x = -3; x <= 3; x++) {
-        for (int y = -3; y <= 3; y++) {
-            Entity* marker = m_scene.addEntity<Entity>(&m_quad);
+    for (int x = -1; x <= 1; x++) {
+        for (int y = -1; y <= 1; y++) {
+            marker = m_scene.addEntity<Entity>(&m_quad);
             marker->setPosition(glm::vec2(x, y));
             marker->setScale(glm::vec2(0.1f));
-            marker->setTexture(&m_grassTexture);  // or any texture
+            marker->setTexture(&m_grassTexture);
         }
     }
 }
 
-void Game::update() {
-    m_camera.setPosition(m_player->getPosition());
+void Game::update(float deltaTime) {
+    if (Input::wasKeyPressed(Key::L)) m_renderer.m_debugDraw = (m_renderer.m_debugDraw) ? false : true;
+    if (Input::wasKeyPressed(Key::F11)) toggleFullscreen();
+}
+
+void Game::onRender(float alpha) {
+    glm::vec2 pos = m_player->getRenderPosition(alpha);
+    glm::vec2 curr = m_camera.getPosition();
+    m_camera.setPosition(curr + (pos - curr) * 0.1f);
 }
--- sandbox/game.h
+++ sandbox/game.h
@@ -8,11 +8,13 @@ class Game : public Sketch {
         Game();
     protected: 
         void start() override;
-        void update() override;
+        void update(float deltaTime) override;
+        void onRender(float alpha) override;
 
     private:
         Texture m_grassTexture { "assets/grass_palette.png" };
         Mesh m_quad = Mesh::createQuad();
         Player* m_player = nullptr;
+        Entity* marker = nullptr;
         const float G = -9.81;
 };
