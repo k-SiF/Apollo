262b4052d1e27886a12bfc3ea9e8fc040cd4a5cf
k-SiF <sifeddine.akaaboune03@gmail.com>
2026-06-21 03:49:03 +0200

    Implement fullscreen toggle/set logic. Added DwmFlush for windowed mode to sync compositor due to OpenGL having micro stutters at 240Hz frame rate on time based updates.

--- engine/src/window.cpp
+++ engine/src/window.cpp
@@ -1,3 +1,8 @@
+#ifdef _WIN32
+#include <dwmapi.h>
+#pragma comment(lib, "dwmapi.lib")
+#endif
+
 #include <apollo/window.h>
 #include <apollo/key.h>
 #include <apollo/input.h>
@@ -13,6 +18,8 @@ namespace apollo {
             case GLFW_KEY_A: return Key::A;
             case GLFW_KEY_S: return Key::S;
             case GLFW_KEY_D: return Key::D;
+            case GLFW_KEY_L: return Key::L;
+            case GLFW_KEY_F11: return Key::F11;
             case GLFW_KEY_UP:    return Key::Up;
             case GLFW_KEY_DOWN:  return Key::Down;
             case GLFW_KEY_LEFT:  return Key::Left;
@@ -41,7 +48,7 @@ namespace apollo {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+        
         m_window = glfwCreateWindow(width, height, title, NULL, NULL);
         if (m_window == NULL) {
             std::cout << "Failed to create GLFW window." << std::endl;
@@ -49,20 +56,52 @@ namespace apollo {
             return;
         }
         glfwMakeContextCurrent(m_window);
-
+        
         if (!gladLoadGL(glfwGetProcAddress)) {
             std::cout << "Failed to initialise GLAD" << std::endl;
             return;
         }
+        
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        glViewport(0, 0, width, height);
+        int fbW, fbH;
+        glfwGetFramebufferSize(m_window, &fbW, &fbH);
+        glViewport(0, 0, fbW, fbH);
+
         glfwSetFramebufferSizeCallback(m_window, framebuffer_size_callback);
         glfwSetKeyCallback(m_window, keyCallback);
+
+        m_windowedWidth = width;
+        m_windowedHeight = height;
+
+        applyVSync();
     }
 
     Window::~Window() {
         glfwDestroyWindow(m_window);
         glfwTerminate();
+    }
+
+    void Window::setFullscreen(bool fullscreen) {
+        if (fullscreen && !m_fullscreen) {
+            // save current windowed pos/size before going fullscreen
+            glfwGetWindowPos(m_window, &m_windowedX, &m_windowedY);
+            glfwGetWindowSize(m_window, &m_windowedWidth, &m_windowedHeight);
+
+            GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+            const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+            glfwSetWindowMonitor(m_window, monitor, 0, 0,
+                                mode->width, mode->height, mode->refreshRate);
+        }
+        else if (!fullscreen && m_fullscreen) {
+            // restore windowed pos/size
+            glfwSetWindowMonitor(m_window, NULL,
+                                m_windowedX, m_windowedY,
+                                m_windowedWidth, m_windowedHeight, 0);
+        }
+        m_fullscreen = fullscreen;
+        applyVSync();
     }
 
     void Window::clear(float r, float g, float b, float a) {
@@ -72,6 +111,12 @@ namespace apollo {
 
     void Window::swapBuffers() {
         glfwSwapBuffers(m_window);
+
+        #ifdef _WIN32
+            if (!m_fullscreen) {
+                DwmFlush();   // sync to compositor in windowed mode
+            }
+        #endif
     }
 
     void Window::pollEvents() {
@@ -91,11 +136,23 @@ namespace apollo {
         return h; 
     }
 
-    float Window::getTime() const {
-        return static_cast<float>(glfwGetTime());
+    double Window::getTime() const {
+        return glfwGetTime();
     }
 
     bool Window::shouldClose() const {
         return glfwWindowShouldClose(m_window);
+    }
+
+    void Window::close() const {
+        glfwSetWindowShouldClose(m_window, true);
+    }
+
+    void Window::applyVSync() const {
+        #ifdef _WIN32
+            glfwSwapInterval(m_fullscreen ? 1 : 0);
+        #else
+            glfwSwapInterval(1);
+        #endif
     }
 }
