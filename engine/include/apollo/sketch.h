d6e06284d7190173f03993c8260f977e6bcec62d
k-SiF <sifeddine.akaaboune03@gmail.com>
2026-06-21 03:42:49 +0200

    Added fixedUpdate, fullscreen setting, and onRender

--- engine/include/apollo/sketch.h
+++ engine/include/apollo/sketch.h
@@ -13,8 +13,12 @@ namespace apollo {
 
         protected:
             virtual void start() {}
-            virtual void update() = 0;
+            virtual void update(float deltaTime) = 0;
+            virtual void fixedUpdate(float deltaTime) {}
+            virtual void onRender(float alpha) {}
             void setBackground(float r, float g, float b) { m_bgR = r; m_bgG = g; m_bgB = b; } ;
+            void setFullscreen(bool fs) { m_window.setFullscreen(fs); }
+            void toggleFullscreen() { m_window.toggleFullscreen(); }
         
         private:
             Window m_window;
